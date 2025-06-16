07f02563e6e27af93310b03e920a9c88188f416f
gabdourado <gab.dourado.if23@gmail.com>
2025-06-16 11:16:52 -0300

    Usando docJS para documentar o código

--- projeto-estadio/CRUD-Torcedor.js
+++ projeto-estadio/CRUD-Torcedor.js
@@ -12,11 +12,6 @@ async function inserir_torcedor(nome, cpf, email, nivel_socio){
         console.warn("Aviso: Presenca de argumento invalido");
         return null;
     }
-
-    if ((!cpf) || (!nome) || (!email) || (!nivel_socio)){
-        console.warn("Aviso: Presenca de argumento invalido.");
-        return null;
-    }
     
     const verifica_cpf = await db.Torcedor.findOne({cpf:cpf})
     
@@ -46,7 +41,12 @@ async function inserir_torcedor(nome, cpf, email, nivel_socio){
         console.error(`Algo de errado aconteceu: ${err}`);
     }
 }
-    
+
+/**
+     * Lista os torcedores cadastrados na base de dados
+     * 
+     * Não recebe parâmetros
+     */
 async function mostrar_torcedores(){
     try{
         res = await db.Torcedor.find();
@@ -54,13 +54,23 @@ async function mostrar_torcedores(){
         console.log("Torcedores listados com sucesso!");
     }
     catch(err){
-        console.error("Algo de errado aconteceu: ${err}");
+        console.error(`Algo de errado aconteceu: ${err}`);
     }
 }
 
+/**
+     * Atualiza os dados de um torcedor, buscando pelo CPF
+     * 
+     * @param {string} cpf - CPF do torcedor para identificacao na DB
+     * @param {string} novo_nome - Novo nome para ser alterado na base de dados
+     * @param {string} novo_cpf - Novo CPF para ser alterado na base de dados
+     * @param {string} novo_email - Novo email para ser alterado na base de dados
+     * @param {string} novo_nivel - Novo nivel de socio para ser alterado na base de dados
+     */
 async function atualizar_torcedor(cpf, novo_nome, novo_cpf, novo_email, novo_nivel){
-    if ((!cpf) || (!novo_cpf) || (!novo_nome) || (!novo_email) || (!novo_nivel)){
-        console.warn("Aviso: Presenca de argumento invalido.");
+    if (typeof cpf != 'string' || typeof novo_nome !=  'string' || typeof novo_cpf != 'string'
+        || typeof novo_email != 'string' || typeof novo_nivel != 'string'){
+        console.warn("Aviso: Presenca de argumento invalido");
         return null;
     }
     
@@ -85,21 +95,28 @@ async function atualizar_torcedor(cpf, novo_nome, novo_cpf, novo_email, novo_niv
     }
 }
 
+/**
+     * Deleta um torcedor da base de dados, buscando pelo CPF
+     * 
+     * @param {string} cpf - CPF do torcedor para identificacao na DB
+     */
 async function deletar_torcedor(cpf){
     if (!cpf){
         console.warn("Aviso: CPF nao fornecido.");
         return null;
     }
+
     const verifica = await db.Torcedor.findOne({cpf: cpf})
+
     if (!verifica){
         console.warn("Aviso: O Torcedor nao encontrado.")
         return null;
     }
     try{
         const res = db.Torcedor.deleteOne({cpf: cpf})
-        console.log("Torcedor ${verifica.nome} removido!")
+        console.log(`Torcedor ${verifica.nome} removido!`)
     }
     catch(err){
-        console.error("Algo de errado aconteceu: ${err}")
+        console.error(`Algo de errado aconteceu: ${err}`)
     }
 }
